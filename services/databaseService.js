fcda40c493ccb141025e86a9365dfc135ba64a82
JorellanaF <00103717@uca.edu.sv>
2022-12-25 01:05:10 -0600

    VAR

--- services/databaseService.js
+++ services/databaseService.js
@@ -3,7 +3,7 @@ const databaseService = () => {
     client: "mysql",
     connection: {
       host: process.env.DB_HOST,
-      port: 3306,
+      port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB,
