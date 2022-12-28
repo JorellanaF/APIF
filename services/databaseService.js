6f947db9da88ad6cc3c3d42936d5ca00081a8c95
JorellanaF <37196790+JorellanaF@users.noreply.github.com>
2022-12-28 17:20:00 -0600

    Update databaseService.js

--- services/databaseService.js
+++ services/databaseService.js
@@ -53,7 +53,7 @@ const databaseService = () => {
       const user = await knex(tabla)
       .column("email as Email", "username as Username")
       .where({email: "email", pass: "password"}).select();
-      const resp = {entro: "ENTRE}
+      const resp = {entro: "ENTRE"}
       return resp;
     } catch (e){
       return res.status(500).json(e);
